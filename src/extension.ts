a0c50269daa75f4c7bfd8acb240e2c1f256488a7
Mathias Gesbert <mathias.gesbert@gmail.com>
2024-10-18 00:39:23 +0200

    feat(console.log): add "Inline Console Log" command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "inline-console-log" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('inline-console-log.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Inline Console Log!');
-	});
-
-	context.subscriptions.push(disposable);
+function inlineConsoleLog() {
+  const editor = vscode.window.activeTextEditor;
+  if (editor === undefined) {
+    return;
+  }
+  editor.edit((editBuilder) => {
+    editor.selections.forEach((selection) => {
+      const selectedValue = editor.document.getText(selection);
+      if (selectedValue.length > 0) {
+        const value = `(console.log(${selectedValue}) ?? ${selectedValue})`;
+        editBuilder.replace(selection, value);
+      } else {
+        const currentLineRange = editor.document.lineAt(
+          selection.active.line
+        ).range;
+        const lineText = editor.document.getText(currentLineRange);
+        const trimmedText = lineText.trim();
+        const startCharacter = lineText.indexOf(trimmedText);
+        const endCharacter = startCharacter + trimmedText.length;
+        const trimmedRange = new vscode.Range(
+          new vscode.Position(currentLineRange.start.line, startCharacter),
+          new vscode.Position(currentLineRange.start.line, endCharacter)
+        );
+        const value = `(console.log(${trimmedText}) ?? ${trimmedText})`;
+        editBuilder.replace(trimmedRange, value);
+      }
+    });
+  });
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "inline-console-log.exec",
+    () => {
+      vscode.window.showInformationMessage(
+        "Hello World from Inline Console Log!"
+      );
+
+      inlineConsoleLog();
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
